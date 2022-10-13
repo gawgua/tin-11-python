bf87804e2b4684ca2f28d01144eadcb31141afa7
gawgua <taisaodeoduoc@gmail.com>
2022-10-13 18:45:14 +0700

    first test

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Sidebar from './lib/Sidebar.svelte'
+</script>
+
+<main>
+  <div class="sidebar">
+    <Sidebar />
+  </div> 
+</main>
+
+<style>
+
+</style>
---
+++ src/lib/Sidebar.svelte
@@ -0,0 +1,17 @@
+<nav>
+    <button>About</button>
+</nav>
+
+<style>
+	nav {
+		position: fixed;
+		top: 0;
+		left: 0;
+		height: 100%;
+		padding: 2rem 1rem 0.6rem;
+		border-left: 3px solid #aaa;
+		background: #fff;
+		overflow-y: auto;
+		width: 15rem;
+	}
+</style>
